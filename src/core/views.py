e05c12c4707f048b3aae36a199da1f97ca29e3d3
RMS <rene.as.himself@gmail.com>
2019-11-11 12:34:25 -0600

    Abstractions using mixins for creating views

--- src/core/views.py
+++ src/core/views.py
@@ -1,16 +1,24 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+import email
+from builtins import object
 
+from django.http import JsonResponse
+from django.shortcuts import render
+from rest_framework import generics, mixins
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
 
 from .models import Post
 from .serializers import PostSerializer
 
+# Generic Response
+# def test_view(APIView):
+#     data = {"name": "john", "age": 23}
+#     return JsonResponse(data)
 
+
+# Using APIView
 class TestView(APIView):
-
     permission_classes = (IsAuthenticated,)
 
     def get(self, request, *args, **kwargs):
@@ -28,6 +36,34 @@ class TestView(APIView):
         return Response(serializer.errors)
 
 
-# def test_view(APIView):
-#     data = {"name": "john", "age": 23}
-#     return JsonResponse(data)
+# Using mixins to minimize code
+class PostView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+        # return Response(serializer.data)
+
+    def perform_create(self, serializer):
+        # send an email
+        serializer.save()
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+# Using even less code using the generic
+# Caveat: only handles POST method, to handle GET use mixins (ListModelMixin)
+class PostCreateView(mixins.ListModelMixin, generics.CreateAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+
+# Even more abstractions using more mixins
+class PostListCreateView(generics.ListCreateAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
--- src/drf_api/urls.py
+++ src/drf_api/urls.py
@@ -13,17 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from core.views import PostCreateView, PostListCreateView, PostView, TestView
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import include, path
 from rest_framework.authtoken.views import obtain_auth_token
-
-from core.views import TestView
 
 urlpatterns = [
     path("api-auth/", include("rest_framework.urls")),
     path("rest-auth/", include("rest_auth.urls")),
     path("admin/", admin.site.urls),
-    path("", TestView.as_view(), name="test"),
+    path("", PostView.as_view(), name="test"),
+    path("create/", PostCreateView.as_view(), name="create"),
+    path("list-create/", PostListCreateView.as_view(), name="list-create"),
     path("api/token/", obtain_auth_token, name="obtain-token"),
 ]
